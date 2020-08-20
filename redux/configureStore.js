88b63e9f4e88053fe1921673f1643f8e7beacc2a
Diane Saia <pontesaia@gmail.com>
2020-08-19 23:13:43 -0400

    Finally got Workshop week2 working

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -5,17 +5,19 @@ import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
+import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            campsites,
-            comments,
-            partners,
-            promotions
-        }),
-        applyMiddleware(thunk, logger)
-    );
+  const store = createStore(
+    combineReducers({
+      campsites,
+      comments,
+      partners,
+      promotions,
+      favorites
+    }),
+    applyMiddleware(thunk, logger)
+  );
 
-    return store;
+  return store;
 }
