829b6376dc14741d2db40732158310c47f9682bc
Diane Saia <pontesaia@gmail.com>
2020-08-22 13:09:11 -0400

    Workshop3-Alert&PAnHandler

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -34,6 +34,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+    const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -61,6 +62,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            }else if (recognizeComment(gestureState)){
+                props.onShowModal()
             }
             return true;
         }
