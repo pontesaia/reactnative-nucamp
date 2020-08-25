ab07b7ece32716fc5fd140452ad2c7006c0d9e4e
Diane Saia <pontesaia@gmail.com>
2020-08-24 22:22:10 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import {
     Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder
+    Alert, PanResponder, Share
 } from 'react-native';
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -69,6 +69,18 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        });
+    };
+
+
+
     if (campsite) {
         return (
             <Animatable.View
@@ -103,6 +115,15 @@ function RenderCampsite(props) {
                             reverse
                             onPress={() => props.onShowModal()}
                             style={styles.cardItem}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            style={styles.cardItem}
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
 
                     </View>
